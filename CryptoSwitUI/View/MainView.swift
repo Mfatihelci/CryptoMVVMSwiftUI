26984f7c9b742a7132df55a5101bd6e56935d5e8
Muhammed fatih Elçi <muhammedfatihelci@192.168.1.35>
2023-12-15 16:11:26 +0300

    sonhali

--- CryptoSwitUI/View/MainView.swift
+++ CryptoSwitUI/View/MainView.swift
@@ -18,10 +18,20 @@ struct MainView: View {
     var body: some View {
         NavigationView {
             
-            List(cryptoListViewModel.cryptoList) { crypto in
-                Text(crypto.currency)
-                Text(crypto.price)
-            }
+            List(cryptoListViewModel.cryptoList,id: \.id) { crypto in
+                VStack {
+                    Text(crypto.currency)
+                        .font(.title3)
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                    Text(crypto.price)
+                        .font(.title2)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                }
+            }.navigationTitle(Text("Crypto Crazy"))
+        }.onAppear {
+            cryptoListViewModel.dowloadCrypts(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
         }
     }
 }
